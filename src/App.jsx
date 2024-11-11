dd2cdb38b49c3bc1784205e9a4587f6c1aea861c
Ayah Ahmed <ayahahmed729@gmail.com>
2024-11-11 13:23:27 +0300

    package form is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { Routes, Route } from 'react-router-dom'
-import './App.css'
-import Project from './components/Project'
-import Service from './components/Service'
-import Calendar from './components/Calendar'
-import Package from './components/Package'
-import Contact from './components/Contact'
-import Policy from './components/Policy'
-import Home from './components/Home'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import { Routes, Route } from "react-router-dom"
+import "./App.css"
+import Project from "./components/Project"
+import Service from "./components/Service"
+import Calendar from "./components/Calendar"
+import Package from "./components/Package"
+import NewPackage from "./components/NewPackage"
+import Contact from "./components/Contact"
+import Policy from "./components/Policy"
+import Home from "./components/Home"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
           <Route path="/projects" element={<Project />} />
           <Route path="/services" element={<Service />} />
           <Route path="/packages" element={<Package />} />
+          <Route path="/new-package" element={<NewPackage />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/book" element={<Calendar />} />
           <Route path="/policy" element={<Policy />} />
