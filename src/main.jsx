5681ba72b9d2cd0ff920fccabdaa135b9c077b18
saraisa12 <saraisa.024@gmail.com>
2024-11-12 00:35:34 +0300

    done

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
+import { GoogleOAuthProvider } from "@react-oauth/google"
+import { createRoot } from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
+import App from "./App.jsx"
 
-createRoot(document.querySelector('#root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+createRoot(document.querySelector("#root")).render(
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+    <BrowserRouter future={{ v7_relativeSplatPath: true }}>
+      <App />
+    </BrowserRouter>
+  </GoogleOAuthProvider>
 )
