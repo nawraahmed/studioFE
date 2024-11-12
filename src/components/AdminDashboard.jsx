58a420d1b24724a604095f1d18e2c53495307e41
NoorAlJirdabi <noor.aljirdabi@gmail.com>
2024-11-12 09:26:52 +0300

    finish admin dashboard

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -3,7 +3,7 @@ import LogsList from './LogsList'
 import UserManagement from './UserManagement'
 import TotalBookings from './TotalBookings'
 
-const AdminDashboard = ({ user }) => {
+const AdminDashboard = () => {
   const [selectedSection, setSelectedSection] = useState('UserManagement') // Default to 'UserManagement'
 
   const renderSection = () => {
@@ -19,51 +19,40 @@ const AdminDashboard = ({ user }) => {
     }
   }
 
-  const navigate = useNavigate()
+  return (
+    <div className="dashboard-container">
+      <h1>Admin Dashboard</h1>
 
-  useEffect(() => {
-    if (user && user.role !== 'admin') {
-      navigate('/') // Redirect to homepage if not an admin
-    }
-  }, [user, navigate])
-
-  if (user?.role === 'admin') {
-    return (
-      <div className="dashboard-container">
-        <h1>Admin Dashboard</h1>
-
-        <div className="dashboard-navigation">
-          <button
-            className={`nav-button ${
-              selectedSection === 'UserManagement' ? 'active' : ''
-            }`}
-            onClick={() => setSelectedSection('UserManagement')}
-          >
-            User Management
-          </button>
-          <button
-            className={`nav-button ${
-              selectedSection === 'TotalBookings' ? 'active' : ''
-            }`}
-            onClick={() => setSelectedSection('TotalBookings')}
-          >
-            Total Bookings
-          </button>
-          <button
-            className={`nav-button ${
-              selectedSection === 'LogsList' ? 'active' : ''
-            }`}
-            onClick={() => setSelectedSection('LogsList')}
-          >
-            Logs
-          </button>
-        </div>
-
-        <div className="dashboard-sections">{renderSection()}</div>
+      <div className="dashboard-navigation">
+        <button
+          className={`nav-button ${
+            selectedSection === 'UserManagement' ? 'active' : ''
+          }`}
+          onClick={() => setSelectedSection('UserManagement')}
+        >
+          User Management
+        </button>
+        <button
+          className={`nav-button ${
+            selectedSection === 'TotalBookings' ? 'active' : ''
+          }`}
+          onClick={() => setSelectedSection('TotalBookings')}
+        >
+          Total Bookings
+        </button>
+        <button
+          className={`nav-button ${
+            selectedSection === 'LogsList' ? 'active' : ''
+          }`}
+          onClick={() => setSelectedSection('LogsList')}
+        >
+          Logs
+        </button>
       </div>
-    )
-  }
-  return null // Render nothing if user is not admin
+
+      <div className="dashboard-sections">{renderSection()}</div>
+    </div>
+  )
 }
 
 export default AdminDashboard
