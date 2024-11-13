dab8712c255cda1613655848b2e28d2f186bcc47
saraisa12 <saraisa.024@gmail.com>
2024-11-13 11:47:47 +0300

    notdone

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import { Routes, Route } from 'react-router-dom'
-import './App.css'
-import Project from './components/Project'
-import Service from './components/Service'
-import NewService from './components/NewService'
-import Calendar from './components/Calendar'
-import Package from './components/Package'
-import NewPackage from './components/NewPackage'
-import Contact from './components/Contact'
-import Policy from './components/Policy'
-import Home from './components/Home'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Register from './components/Register'
-import SignIn from './components/SignIn'
-import AdminDashboard from './components/AdminDashboard'
-import Protofolio from './components/Protofolio'
-import PortfolioPage from './components/PortfolioPage'
-import { useState, useEffect } from 'react'
-import { CheckSession } from './services/Auth'
+import { Routes, Route } from "react-router-dom"
+import "./App.css"
+import Project from "./components/Project"
+import Service from "./components/Service"
+import NewService from "./components/NewService"
+import Calendar from "./components/Calendar"
+import Package from "./components/Package"
+import NewPackage from "./components/NewPackage"
+import Contact from "./components/Contact"
+import Policy from "./components/Policy"
+import Home from "./components/Home"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Register from "./components/Register"
+import SignIn from "./components/SignIn"
+import AdminDashboard from "./components/AdminDashboard"
+import Protofolio from "./components/Protofolio"
+import PortfolioPage from "./components/PortfolioPage"
+import { useState, useEffect } from "react"
+import { CheckSession } from "./services/Auth"
 import Profile from "./components/Profile"
+import AdminCalendar from "./components/AdminCalendar"
 
 function App() {
   const [user, setUser] = useState(null)
@@ -61,7 +62,9 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/protofolio" element={<Protofolio />} />
           <Route path="/profile" element={<Profile user={user} />} />
-          <Route path="/portfolio-list" element={<PortfolioPage />} />{" "}
+          <Route path="/profile" element={<Profile user={user} />} />
+          <Route path="/portfolio-list" element={<PortfolioPage />} />
+          <Route path="/admin-calendar" element={<AdminCalendar />} />
         </Routes>
       </main>
       <Footer />
