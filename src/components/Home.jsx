bcdc4ae5b0f26b3b79a5906c5641ea3bc8e4666c
Zahralsayed <zalmatooq31@gmail.com>
2024-11-13 18:04:17 +0300

    merge

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,37 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import { useTranslation } from "react-i18next"
 import Protofolio from "./Protofolio"
+import "../static/home.css"
+import { useTranslation } from "react-i18next"
 
 const Home = () => {
   const { t } = useTranslation()
 
   return (
     <div className="home">
+      <p className="home-p">
+        Professional branding services tailored to make your
+        <br />
+        business stand out. Book an appointment and
+        <br />
+        explore our work.
+      </p>
+
+      <button className="book-session-btn">Book A Session!</button>
+
+      <br />
+
+      <div className="separator">
+        <span>_________________or_________________</span>
+      </div>
+
+      <br />
+
       <Link to="/portfolio-list" className="portfolio-link">
         {t("view_our_portfolio")}
       </Link>
+      <br />
+      <h1 className="featured-title">Featured Projects</h1>
       <h1> {t("featured_projects")}</h1>
       <Protofolio />
     </div>
